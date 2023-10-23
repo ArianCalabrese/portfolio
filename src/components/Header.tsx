ef8d64087b60c58e44e76834151b92f742d0387b
ArianCalabrese <ariancalabrese@hotmail.com>
2023-10-23 19:53:09 -0300

    First version of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,38 @@
 import React from "react";
+import home from "@/icons/home.svg";
+import user from "@/icons/user.svg";
+import skill from "@/icons/skill.svg";
+import certification from "@/icons/certification.svg";
+import portfolio from "@/icons/portfolio.svg";
+import social from "@/icons/social.svg";
+import Image from "next/image";
 
 type Props = {};
+
+const headerData = [
+  { icon: home, name: "Home" },
+  { icon: user, name: "About me" },
+  { icon: skill, name: "Skills" },
+  { icon: certification, name: "Certifications" },
+  { icon: portfolio, name: "Portfolio" },
+  { icon: social, name: "Social" },
+];
 
 const Header = (props: Props) => {
   return (
     <header>
-      <ul className="absolute grid grid-cols-10 w-full justify-center items-center bg-red-600">
-        <li>Home</li>
-        <li>About me</li>
-        <li>Skills</li>
-        <li>Certifications</li>
-        <li>Portfolio</li>
-        <li>Social</li>
-      </ul>
+      <nav className="absolute w-full my-10 rounded-lg">
+        <ul className="relative grid grid-cols-6 gap-4 px-10 rounded-lg mx-32 bg-orange-600">
+          {headerData.map((item, index) => (
+            <li>
+              <button className="flex items-center hover:bg-orange-300 hover:shadow-inner hover:shadow-neutral-500 rounded-md p-4">
+                <Image src={item.icon} alt={item.name} width={32} />
+                <p className="text-xl">{item.name}</p>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
---
+++ src/static/data.ts
@@ -0,0 +1,10 @@
+
+
+export const headerData = [
+  { name: "Home" },
+  { name: "About me" },
+  { name: "Skills" },
+  { name: "Certifications" },
+  { name: "Portfolio" },
+  { name: "Social" },
+];
