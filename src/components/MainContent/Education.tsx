75056e0d87ede1192a2d04f66d3eb737bc14bedb
ArianCalabrese <ariancalabrese@hotmail.com>
2024-05-01 15:15:43 -0300

    Saving code

--- src/components/MainContent/Education.tsx
+++ src/components/MainContent/Education.tsx
@@ -8,6 +8,11 @@ const Education = (props: Props) => {
   return (
     <div className="flex flex-col gap-6">
       <p className="text-5xl">Educación</p>
+      <div className="relative flex py-5 items-center">
+        <div className="flex-grow border-t border-gray-400"></div>
+        <span className="flex-shrink mx-4 text-gray-400">Universitaria</span>
+        <div className="flex-grow border-t border-gray-400"></div>
+      </div>
       <div className="flex">
         <div className="p-2">
           <div className="rounded-full bg-white p-2">
@@ -38,6 +43,11 @@ const Education = (props: Props) => {
           </div>
         </div>
       </div>
+      <div className="relative flex py-5 items-center">
+        <div className="flex-grow border-t border-gray-400"></div>
+        <span className="flex-shrink mx-4 text-gray-400">Cursos</span>
+        <div className="flex-grow border-t border-gray-400"></div>
+      </div>
       <div className="flex">
         <div className="p-2">
           <div className="rounded-full bg-white p-2">
@@ -46,17 +56,13 @@ const Education = (props: Props) => {
         </div>
         <div className="p-2">
           <div>
-            <p className="font-medium text-3xl">
-              Universidad Tecnologica Nacional
-            </p>
+            <p className="font-medium text-3xl">Coursera</p>
           </div>
           <div>
-            <p className="font-light text-2xl">
-              Ingeniero en Sistemas de la Información
-            </p>
+            <p className="font-light text-2xl">Nombre del curso</p>
           </div>
           <div>
-            <p className="font-extralight text-xl">2017-2023</p>
+            <p className="font-extralight text-xl">2017</p>
           </div>
           <div>
             <p>
